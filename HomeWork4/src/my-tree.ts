46bb2ea4db7ac6a47bbc4741fe7556744dadfb69
Myrzabek95 <kzmyrzabek@gmail.com>
2021-08-09 21:46:14 +0600

    hw4

---
+++ HomeWork4/src/my-tree.ts
@@ -0,0 +1,30 @@
+const tmplTree: HTMLTemplateElement = document.createElement('template');
+
+tmplTree.innerHTML = `
+  <style>
+    .tree {
+      background: #966F33;
+      width: 15px;
+      padding: 5px;
+      border: 2px solid grey;
+      margin: 10px 25px;
+      text-align: center;
+      color: yellow;
+    }
+  </style>
+  <div class="tree">
+      <slot name="treeId"></slot>
+      <slot></slot>
+  </div>
+`;
+
+export class MyTree extends HTMLElement {
+  connectedCallback(): void {
+    const shadowRoot = this.attachShadow({mode: "open"});
+    shadowRoot.appendChild(tmplTree.content.cloneNode(true));
+    const treeElem = shadowRoot.querySelector('slot[name="treeId"]');
+    treeElem.innerHTML = this.getAttribute('treeId');
+  };
+};
+
+customElements.define('my-tree', MyTree);
