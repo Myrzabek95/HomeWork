c4ea6978311b0d013f2f969bad4f3515168a1bbc
Myrzabek95 <kzmyrzabek@gmail.com>
2021-08-12 22:30:42 +0600

    HW5

---
+++ HomeWork5/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+let result = {
+    files: [],
+    dirs: []
+}
+
+const MainPath = process.argv.slice(2)[0];
+
+async function Main(MainPath){ 
+    if (!MainPath){
+        console.log('Wrong Path');
+        return;
+    }
+    MainPath = MainPath.slice(0, -1);
+    result.dirs.push(MainPath);
+    let r = await getDirContent(MainPath);
+    console.log(result);
+}
+
+async function getDirContent(FolderPath){
+    FolderPath = FolderPath + '/';  
+    let files = await fs.promises.readdir(FolderPath)
+    for await (const file of files){
+        let current_path = FolderPath + file; 
+        let stat = await fs.promises.lstat(current_path);
+        if(stat.isFile())
+            result.files.push(current_path);
+        else{
+            result.dirs.push(current_path);
+            let d = await getDirContent(current_path);
+        }
+    }
+}
+
+Main(MainPath);
